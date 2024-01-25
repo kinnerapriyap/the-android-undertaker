836e7825fca934aeb49ebb10caf7195dba8b66f4
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2024-01-25 21:47:41 +0100

    Use animation in home screen list

--- app/src/main/java/com/kinnerapriyap/undertaker/Undertake.kt
+++ app/src/main/java/com/kinnerapriyap/undertaker/Undertake.kt
@@ -3,14 +3,17 @@ package com.kinnerapriyap.undertaker
 import androidx.annotation.StringRes
 import com.roudikk.guia.core.NavigationKey
 
-data class Undertake(
-    @StringRes val title: Int,
+sealed interface Undertake {
+    @get:StringRes
+    val title: Int
     val navigationKey: NavigationKey
-)
+
+    data class DottyCirclesAnimationUndertake(
+        override val title: Int = R.string.dotty_circles_animation_title,
+        override val navigationKey: NavigationKey = DottyCirclesAnimationKey
+    ) : Undertake
+}
 
 internal val undertakes = listOf(
-    Undertake(
-        title = R.string.dotty_circles_animation_title,
-        navigationKey = DottyCirclesAnimationKey,
-    )
+    Undertake.DottyCirclesAnimationUndertake()
 )
--- app/src/main/java/com/kinnerapriyap/undertaker/ui/composables/HomeScreen.kt
+++ app/src/main/java/com/kinnerapriyap/undertaker/ui/composables/HomeScreen.kt
@@ -1,12 +1,14 @@
 package com.kinnerapriyap.undertaker.ui.composables
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -33,11 +35,28 @@ fun HomeScreen(
             contentPadding = PaddingValues(vertical = 8.dp, horizontal = 16.dp)
         ) {
             items(undertakes) { undertake ->
-                OutlinedButton(
-                    onClick = { navigator.push(undertake.navigationKey) }
-                ) {
-                    Text(text = stringResource(undertake.title))
-                }
+                ListItem(
+                    headlineContent = {
+                        Text(
+                            text = stringResource(undertake.title),
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                    },
+                    leadingContent = {
+                        when (undertake) {
+                            is Undertake.DottyCirclesAnimationUndertake ->
+                                DottyCirclesAnimation(
+                                    modifier = Modifier
+                                        .clickable { navigator.push(undertake.navigationKey) },
+                                    gridSize = 3,
+                                    gridSpacing = 8.dp,
+                                    circleSize = 12.dp,
+                                    offsetSize = 2.dp
+                                )
+                        }
+                    },
+                    shadowElevation = 2.dp
+                )
             }
         }
     }
