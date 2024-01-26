7751ff41800e659679c94501fccf300d4b7b5f05
Kinnera Priya Putti <kinnerapriyap@gmail.com>
2024-01-26 08:49:47 +0100

    Add back nav icon to screen

--- app/src/main/java/com/kinnerapriyap/undertaker/ui/composables/DottyCirclesAnimationScreen.kt
+++ app/src/main/java/com/kinnerapriyap/undertaker/ui/composables/DottyCirclesAnimationScreen.kt
@@ -23,6 +23,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -34,11 +36,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.kinnerapriyap.undertaker.R
+import com.roudikk.guia.extensions.pop
+import com.roudikk.guia.extensions.requireLocalNavigator
 import kotlin.math.roundToInt
 import kotlin.random.Random
 
@@ -47,10 +52,21 @@ import kotlin.random.Random
 fun DottyCirclesAnimationScreen(
     modifier: Modifier = Modifier
 ) {
+    val navigator = requireLocalNavigator()
     Scaffold(
         modifier = modifier.navigationBarsPadding(),
         topBar = {
-            TopAppBar(title = { Text(stringResource(R.string.dotty_circles_animation_title)) })
+            TopAppBar(
+                title = { Text(stringResource(R.string.dotty_circles_animation_title)) },
+                navigationIcon = {
+                    IconButton(onClick = { navigator.pop() }) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_arrow_back),
+                            contentDescription = stringResource(R.string.arrow_back_content_description)
+                        )
+                    }
+                }
+            )
         }
     ) {
         Box(
